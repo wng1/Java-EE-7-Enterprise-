58aca604499065a6aff688cbe3d3b1b58193c9d6
wng1 <wng1@users.noreply.github.com>
2016-03-20 14:14:51 +0000

    Create TestUser.java

---
+++ test/TestUser.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestUser
+{
+
+User user = new User();
+Order o = new Order();
+
+
+public void testUser() 
+{
+user.setFirstName("John")
+user.setLastName("Smith")
+
+double quantity = order.calculateTotal(User);
+assertEquals(12, quantity, 0);
+
+}
+
+}
