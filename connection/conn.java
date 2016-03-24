f91e9e4cadf85562f6ec56f1ecf8f0a7a3d923fe
wng1 <wng1@users.noreply.github.com>
2016-03-24 18:59:16 +0000

    Update conn.java

--- connection/conn.java
+++ connection/conn.java
@@ -10,6 +10,21 @@ try
 URL n = new URL(args[0]); //open the URLConnection for reading
 URLConnection uc = n.openConnection(); //access to the HTTP header
 
+try (InputStream in = uc.getInputStream())
+{
+InputStream buffer = new BufferedInputStream(in);
+
+Reader r = new InputStreamReader(buffer);
+
+int c;
+while ((c = r.read()) != -1)
+{
+  System.out.println((char) c;
+}
+} catch (MalformedURLException ex)
+{
+  System.err.println(ex)
+}
 }
 
 }
