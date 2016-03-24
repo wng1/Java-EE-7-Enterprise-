84f6c026bd613450438884643900f3e448b2e438
wng1 <wng1@users.noreply.github.com>
2016-03-24 18:27:57 +0000

    Create conn.java

---
+++ connection/conn.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.net.*;
+
+public class conn
+{
+
+public static void main(String[] args)
+{
+try
+URL n = new URL(args[0]); //open the URLConnection for reading
+URLConnection uc = n.openConnection(); //access to the HTTP header
+
+}
+
+}
